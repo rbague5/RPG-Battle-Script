af8b6f6b0118948edf131ab135c636ea3bca934e
Deltoidex <rogerbm26@gmail.com>
2020-01-03 16:14:29 +0100

    Whitespace in hp and mp

--- classes/game.py
+++ classes/game.py
@@ -89,18 +89,45 @@ class Person:
 			bar_ticks -= 1
 
 		while len(hp_bar) < 25:
-			hp_bar += "     "
+			hp_bar += " "
 
 		while mp_ticks > 0:
 			mp_bar += "█"
 			mp_ticks -= 1
 
 		while len(mp_bar) < 10:
-			mp_bar += "     "
+			mp_bar += " "
 
+		hp_string = str(self.hp) + "/" + str(self.maxhp)
+		current_hp = ""
 
-		print("                         ___________________________________________                 _________________")
+		if len(hp_string) < 9:
+			decreased = 9 - len(hp_string)
+
+			while decreased > 0:
+				current_hp += " "
+				decreased -= 1
+
+			current_hp += hp_string
+		else:
+			current_hp = hp_string
+
+		mp_string = str(self.mp) + "/" + str(self.maxmp)
+		current_mp = ""
+
+		if len(mp_string) < 9:
+			decreased = 9 - len(mp_string)
+
+			while decreased > 0:
+				current_mp += " "
+				decreased -= 1
+
+			current_mp += mp_string
+		else:
+			current_mp = mp_string
+
+		print("                        ___________________________________________                  _________________")
 		print(bcolors.BOLD + self.name + "      " +
-			str(self.hp) + "/" + str(self.maxhp) + "  |" + bcolors.OKGREEN + hp_bar + bcolors.ENDC +"|      " +
-			str(self.mp) + "/" + str(self.maxmp) + "  |" + bcolors.OKBLUE + mp_bar + bcolors.ENDC + "|")
+			current_hp + " |" + bcolors.OKGREEN + hp_bar + bcolors.ENDC +"|      " +
+			current_mp + " |" + bcolors.OKBLUE + mp_bar + bcolors.ENDC + "|")
 
